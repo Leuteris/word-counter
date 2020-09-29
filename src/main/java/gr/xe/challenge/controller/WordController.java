a13ed532ce61e5be4f1c11225cc6aff2106f6e64
Lefteris Katiforis <leuteriskatiforis@gmail.com>
2020-09-29 17:09:39 +0300

    test 4k

--- src/main/java/gr/xe/challenge/controller/WordController.java
+++ src/main/java/gr/xe/challenge/controller/WordController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-
+//sss
 @RestController
 @RequestMapping("/word")
 public class WordController {
