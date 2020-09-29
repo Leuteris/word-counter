aaf3b881afff8bb81339191e9bc958c407b5a35f
Lefteris Katiforis <leuteriskatiforis@gmail.com>
2020-09-29 17:35:26 +0300

    test 4k

--- src/main/java/gr/xe/challenge/controller/WordController.java
+++ src/main/java/gr/xe/challenge/controller/WordController.java
@@ -18,7 +18,7 @@ public class WordController {
     @Autowired
     WordService wordService;
 
-    @PostMapping("/count")
+    @PostMapping("/count")//
     public WordCountResponse count(@Valid @RequestBody WordCountRequest request) {
         return wordService.countWords(request.getText());
     }
